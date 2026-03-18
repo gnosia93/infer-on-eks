78b4ad0c7cf1ce1db12bb2246b4aa042dd7fc9f7
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2026-03-18 18:31:57 +0900

    Update outputs.tf

--- iac/tf/outputs.tf
+++ iac/tf/outputs.tf
@@ -1,4 +1,3 @@
-output "vscode_url" {
-    value = "http://${aws_instance.x86_box.public_dns}:8080"
-    description = "vscode URL"
+output "vscode" {
+  value = "http://${aws_instance.gpu.public_ip}:8080"
 }
