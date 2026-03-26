b4a7e5a895852d8524d2be01e5d782d8c8183bc5
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2026-03-26 17:51:20 +0900

    Update eks.tf

--- iac/tf/eks.tf
+++ iac/tf/eks.tf
@@ -21,7 +21,7 @@ resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
 resource "aws_eks_cluster" "main" {
   name     = var.cluster_name
   role_arn = aws_iam_role.eks_cluster_role.arn
-  version  = "1.32"
+  version  = "1.34"
 
   vpc_config {
     subnet_ids              = concat(aws_subnet.public[*].id, aws_subnet.private[*].id)
